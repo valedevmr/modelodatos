ba807300e9652ad001adec91f647447e1c5f6bd4
Valente Montero <devcommartinez91@gmail.com>
2025-05-22 01:31:20 -0600

    add method delete

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -11,4 +11,10 @@ class ReservationController extends Controller
 
         return response()->json(["success"=>true,"message"=>"bien desde el index enpoint"],200);
     }
+
+
+    public function destroy(Request $request){
+
+        return response()->json(["success"=>true,"message"=>"DElete method"],200);
+    }
 }
